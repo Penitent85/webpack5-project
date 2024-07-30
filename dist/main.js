9784219478b34609dbbd7613d96d328a696ae0d4
Mahdi <penitent85@gmail.com>
2024-07-30 11:57:24 +0300

    add

--- dist/main.js
+++ dist/main.js
@@ -1,24 +1,77 @@
-/******/ (() => { // webpackBootstrap
-/*!**********************!*\
-  !*** ./src/index.js ***!
-  \**********************/
-function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
-function ownKeys(e, r) { var t = Object.keys(e); if (Object.getOwnPropertySymbols) { var o = Object.getOwnPropertySymbols(e); r && (o = o.filter(function (r) { return Object.getOwnPropertyDescriptor(e, r).enumerable; })), t.push.apply(t, o); } return t; }
-function _objectSpread(e) { for (var r = 1; r < arguments.length; r++) { var t = null != arguments[r] ? arguments[r] : {}; r % 2 ? ownKeys(Object(t), !0).forEach(function (r) { _defineProperty(e, r, t[r]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(t)) : ownKeys(Object(t)).forEach(function (r) { Object.defineProperty(e, r, Object.getOwnPropertyDescriptor(t, r)); }); } return e; }
-function _defineProperty(e, r, t) { return (r = _toPropertyKey(r)) in e ? Object.defineProperty(e, r, { value: t, enumerable: !0, configurable: !0, writable: !0 }) : e[r] = t, e; }
-function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
-function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
-var elvenShieldRecipe = {
-  leatherStrips: 2,
-  ironIngot: 1,
-  refinedMoonstone: 4
-};
-var elvenGrantletsRecipe = _objectSpread(_objectSpread({}, elvenShieldRecipe), {}, {
-  leather: 1,
-  refinedMoonstone: 4
-});
-console.log(elvenShieldRecipe);
-console.log(elvenGrantletsRecipe);
-/******/ })()
-;
+!(function () {
+  "use strict";
+  function e(t) {
+    return (
+      (e =
+        "function" == typeof Symbol && "symbol" == typeof Symbol.iterator
+          ? function (e) {
+              return typeof e;
+            }
+          : function (e) {
+              return e &&
+                "function" == typeof Symbol &&
+                e.constructor === Symbol &&
+                e !== Symbol.prototype
+                ? "symbol"
+                : typeof e;
+            }),
+      e(t)
+    );
+  }
+  function t(e, t) {
+    var r = Object.keys(e);
+    if (Object.getOwnPropertySymbols) {
+      var o = Object.getOwnPropertySymbols(e);
+      t &&
+        (o = o.filter(function (t) {
+          return Object.getOwnPropertyDescriptor(e, t).enumerable;
+        })),
+        r.push.apply(r, o);
+    }
+    return r;
+  }
+  function r(e) {
+    for (var r = 1; r < arguments.length; r++) {
+      var n = null != arguments[r] ? arguments[r] : {};
+      r % 2
+        ? t(Object(n), !0).forEach(function (t) {
+            o(e, t, n[t]);
+          })
+        : Object.getOwnPropertyDescriptors
+        ? Object.defineProperties(e, Object.getOwnPropertyDescriptors(n))
+        : t(Object(n)).forEach(function (t) {
+            Object.defineProperty(e, t, Object.getOwnPropertyDescriptor(n, t));
+          });
+    }
+    return e;
+  }
+  function o(t, r, o) {
+    return (
+      (r = (function (t) {
+        var r = (function (t) {
+          if ("object" != e(t) || !t) return t;
+          var r = t[Symbol.toPrimitive];
+          if (void 0 !== r) {
+            var o = r.call(t, "string");
+            if ("object" != e(o)) return o;
+            throw new TypeError("@@toPrimitive must return a primitive value.");
+          }
+          return String(t);
+        })(t);
+        return "symbol" == e(r) ? r : r + "";
+      })(r)) in t
+        ? Object.defineProperty(t, r, {
+            value: o,
+            enumerable: !0,
+            configurable: !0,
+            writable: !0,
+          })
+        : (t[r] = o),
+      t
+    );
+  }
+  var n = { leatherStrips: 2, ironIngot: 1, refinedMoonstone: 4 },
+    i = r(r({}, n), {}, { leather: 1, refinedMoonstone: 4 });
+  console.log(n), console.log(i);
+})();
 //# sourceMappingURL=main.js.map
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,40 @@
-const path = require('path');
-let mode ='development';
-if(process.env.NODE_ENV === 'production'){
-  mode = 'production';
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+let mode = "development";
+if (process.env.NODE_ENV === "production") {
+  mode = "production";
 }
 
-
-module.exports={
-     mode: mode, 
-     module:{
-      rules:[
-         {
-            test:/\.js$/,
-            exclude:/node_modules/,
-            use: {
-               loader: 'babel-loader',
-            }
-         },
-        
-      ]
-     },
-    
-     devtool: 'source-map',
-     devServer: {
-      static: {
-        directory: path.join(__dirname, './dist'),
+module.exports = {
+  mode: mode,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
       },
-      compress: true,
-      port: 9000,
+      {
+        test: /\.s?css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [new MiniCssExtractPlugin()],
+  devtool: "source-map",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "./dist"),
     },
-} ;
-
+    compress: true,
+    hot: true,
+    port: 9000,
+  },
+};
